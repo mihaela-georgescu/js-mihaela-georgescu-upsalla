f66857400d94ee1f88144bdd35dc05d688de7c18
Mihaela Georgescu <mihaela.georgescu50@gmail.com>
2023-09-18 10:55:42 +0300

    S03

---
+++ m01s03/ex07/app.js
@@ -0,0 +1,139 @@
+console.warn(`
+Creeaza un obiect numit person
+Obiectul sa contina urmatoarele proprietati: firstName, lastName, email, birthYear,
+pets, zipCode.
+Proprietatea pets sa contina un array de obiecte cu trei pozitii.
+Obiectele din pets sa contina urmatoarele proprietati: name, species, age.
+`);
+var person = {
+  firstName: 'Steven',
+  lastName: 'Stevenson',
+  email: 'steven.stevenson@gmail.com',
+  birthYear: 1994,
+  zipCode: '12345',
+  pets: [
+    {
+      name: 'Tucker',
+      species: 'caine',
+      age: 6,
+    },
+    {
+      name: 'Nimbus',
+      species: 'pisica',
+      age: 4,
+    },
+    {
+      name: 'Ruby',
+      species: 'caine',
+      age: 5,
+    },
+  ],
+};
+
+console.warn(`
+Afiseaza propozitia: “Numele meu este: xxx yyy si am x animale.”. Nu uita de proprietatea
+length a arrayului pets.
+`);
+console.log(
+  'Numele meu este: ' +
+    person.firstName +
+    ' ' +
+    person.lastName +
+    ' si am ' +
+    person.pets.length +
+    ' animale.',
+);
+
+console.warn(`
+Afiseaza propozitia: “Am acelasi email din copilarie: xxx.”.
+`);
+console.log('Am acelasi email din copilarie: ' + person.email + '.');
+
+console.warn(`
+Afiseaza propozitia: “Unul din cele x animale ale mele este species si are age ani.”
+`);
+console.log(
+  'Unul din cele ' +
+    person.pets.length +
+    ' animale ale mele este ' +
+    person.pets[1].species +
+    ' si are ' +
+    person.pets[1].age +
+    ' ani.',
+);
+
+console.warn(`
+Calculeaza si afiseaza (folosind anul curent), anul de nastere al animalului de pe indexul 2.
+`);
+console.log((2023 - person.pets[2].age).toString());
+
+console.warn(`
+Calculeaza si salveaza in variabila difference diferenta de ani dintre persoana si animalul
+de pe pozitia 0 si afiseaza aceasta diferenta. Foloseste anul curent.
+`);
+var difference = 2023 - person.birthYear - person.pets[0].age;
+console.log(difference.toString());
+
+console.warn(`
+Salveaza numele animalului de pe indexul 0 intr-o variabila numita petName.
+`);
+var petName = person.pets[0].name;
+
+console.warn(`
+Afiseaza propozitia: “Intre firstName si petName este o diferenta de difference ani.”.
+`);
+console.log(
+  'Intre ' +
+    person.firstName +
+    ' si ' +
+    petName +
+    ' este o diferenta de ' +
+    difference +
+    ' ani.',
+);
+
+var prop01Element = document.getElementById('prop01');
+var petname1 = person.pets[0].name;
+var petname2 = person.pets[1].name;
+var petname3 = person.pets[2].name;
+var textToShow01 =
+  person.firstName +
+  ', ' +
+  petname1 +
+  ', ' +
+  petname2 +
+  ', ' +
+  petname3 +
+  ' locuiesc toti in aceeasi casa';
+prop01Element.textContent = textToShow01;
+
+var prop02Element = document.getElementById('prop02');
+var age1 = person.pets[0].age;
+var age2 = person.pets[1].age;
+var age3 = person.pets[2].age;
+var textToShow02 = age1 - age3;
+prop02Element.textContent = textToShow02;
+
+var prop03Element = document.getElementById('prop03');
+var textToShow03 =
+  'Ma numesc ' +
+  person.firstName +
+  ' ' +
+  person.lastName +
+  ', m-am nascut in ' +
+  person.birthYear +
+  ' si codul meu postal este: ' +
+  person.zipCode;
+prop03Element.textContent = textToShow03;
+
+var prop04Element = document.getElementById('prop04');
+var textToShow04 =
+  'Animalele mele s-au nascut in ' +
+  (2023 - age1) +
+  ', ' +
+  (2023 - age2) +
+  ', ' +
+  ' respectiv ' +
+  (2023 - age3) +
+  '.';
+prop04Element.textContent = textToShow04;
---
+++ m01s03/ex08/app.js
@@ -0,0 +1,128 @@
+var cart = {
+  id: 'cart-id-12345',
+  ownerId: 'user-id-54321',
+  firstName: 'Labrador',
+  lastName: 'Retriever',
+  email: 'labrador.retriever@gmail.com',
+  products: [
+    {
+      id: 'product-id-0',
+      name: 'Mouse',
+      price: 200,
+      quantity: 2,
+    },
+    {
+      id: 'product-id-1',
+      name: 'Keyboard',
+      price: 480,
+      quantity: 1,
+    },
+    {
+      id: 'product-id-2',
+      name: 'Keyboard Mat',
+      price: 119,
+      quantity: 1,
+    },
+  ],
+};
+
+console.warn(`
+Afiseaza propozitia “Numele meu este xxx yyy iar emailul meu este: eee.”
+`);
+console.log(
+  'Numele meu este ' +
+    cart.firstName +
+    ' ' +
+    cart.lastName +
+    ' iar emailul meu este: ' +
+    cart.email +
+    '.',
+);
+
+console.warn(`
+Afiseaza propozitia “Utilizatorul cu idul iii are xxx tipuri de produse in cartul cu
+idul ccc.” folosind proprietatea length pe arrayul de produse
+`);
+console.log(
+  'Utilizatorul cu idul ' +
+    cart.ownerId +
+    ' are ' +
+    cart.products.length +
+    ' tipuri de produse in cartul cu idul ' +
+    cart.id +
+    '.',
+);
+
+console.warn(`
+Afiseaza propozitia “Al treilea produs pe care il cumpara xxx este ppp.”
+`);
+console.log(
+  'Al treilea produs pe care il cumpara ' +
+    cart.firstName +
+    ' este ' +
+    cart.products[2].name +
+    '.',
+);
+
+console.warn(`
+Afiseaza propozitia “xxx yyy cumpara ppp produse in total”; folosind accesul direct
+la fiecare din obiectele din arrayul products.
+`);
+console.log(
+  cart.firstName +
+    ' ' +
+    cart.lastName +
+    ' cumpara ' +
+    (cart.products[0].quantity +
+      cart.products[1].quantity +
+      cart.products[2].quantity) +
+    ' produse in total.',
+);
+
+console.warn(`
+Afiseaza propozitia “Sunt xxx si cumpar produsele p1, p2 si p3.”; folosind accesul
+direct la fiecare din obiectele din arrayul products.
+`);
+console.log(
+  'Sunt ' +
+    cart.firstName +
+    ' si cumpar produsele ' +
+    cart.products[0].name +
+    ', ' +
+    cart.products[1].name +
+    ' si ' +
+    cart.products[2].name +
+    '.',
+);
+
+console.warn(`
+Afiseaza propozitia “Valoarea totala a cartului cu idul iii este ttt.”; folosind
+accesul direct la fiecare din obiectele din arrayul products
+(price, quantity). (product[0].price * product[0].quantity )
+`);
+console.log(
+  'Valoarea totala a cartului cu idul ' +
+    cart.id +
+    ' este ' +
+    (cart.products[0].price * cart.products[0].quantity +
+      cart.products[1].price * cart.products[1].quantity +
+      cart.products[2].price * cart.products[2].quantity) +
+    '.',
+);
+
+console.warn(`
+Afiseaza propozitia ”xxx yyy a cumparat produsul ppp in cantitatea ccc in valoare
+totala de ttt.” Foloseste produsul de pe indexul 0.
+`);
+console.log(
+  cart.firstName +
+    ' ' +
+    cart.lastName +
+    ' a cumparat produsul ' +
+    cart.products[0].name +
+    ' in cantitatea ' +
+    cart.products[0].quantity +
+    ' in valoare totala de ' +
+    cart.products[0].price * cart.products[0].quantity +
+    '.',
+);
